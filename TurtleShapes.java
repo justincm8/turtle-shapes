623f6fbc9498f7cb68b740a4dfc1201e5f9674c1
justincm8 <80138218+justincm8@users.noreply.github.com>
2024-09-14 21:12:24 -0400

    turtle-shapes assignment starter

---
+++ TurtleShapes.java
@@ -0,0 +1,15 @@
+import static twrapper.TurtleWrapper.*;
+
+void main() {
+    penColor("red");
+    speed(150);
+
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+}
---
+++ twrapper/TurtleWrapper.java
@@ -0,0 +1,254 @@
+package twrapper;
+
+public class TurtleWrapper {
+    private static Turtle t = new Turtle();
+    static {
+        t.speed(200);
+    }
+
+     /**
+     * Pauses the program for a given amount of time.
+     * 
+     * @param time the time to wait in miliseconds
+     */
+    private static void wait(int time) 
+    {
+        try 
+        {
+            Thread.sleep(time);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+
+    /**
+     * Sets the speed of the animation.
+     *
+     * @param delay milliseconds it takes to do one action
+     * @return state change timestamp
+     */
+    public static long speed(double delay) {
+        return t.speed(delay);
+    }
+
+    /**
+     * Moves the turtle forward by the distance.
+     *
+     * @param distance distance to move forward
+     * @return state change timestamp
+     */
+    public static long forward(double distance) {
+        return t.forward(distance);
+    }
+
+    /**
+     * Moves the turtle backward by the distance.
+     *
+     * @param distance distance to move backward
+     * @return state change timestamp
+     */
+    public static long backward(double distance) {
+        return t.backward(distance);
+    }
+
+    /**
+     * Turns the turtle left by the number of indicated degrees.
+     *
+     * @param angle angle in degrees
+     * @return state change timestamp
+     */
+    public static long left(double angle) {
+        return t.left(angle);
+    }
+
+    /**
+     * Turns the turtle right by the number of indicated degrees.
+     *
+     * @param angle angle in degrees
+     * @return state change timestamp
+     */
+    public static long right(double angle) {
+        return t.right(angle);
+    }
+
+    /**
+     * Gets the direction the turtle is facing neglecting tilt.
+     *
+     * @return state change timestamp
+     */
+    public static double getDirection() {
+        return t.getDirection();
+    }
+
+    /**
+     * Sets the direction the turtle is facing neglecting tilt.
+     *
+     * @param direction angle counter-clockwise from east
+     * @return state change timestamp
+     */
+    public static long setDirection(double direction) {
+        return t.setDirection(direction);
+    }
+
+    /**
+     * Moves the turtle to (0,0) and facing east.
+     *
+     * @return state change timestamp
+     */
+    public long home() {
+        return t.home();
+    }
+
+    /**
+     * Sets the direction in such a way that it faces (x,y)
+     *
+     * @param x x coordinate of target location
+     * @param y y coordinate of target location
+     * @return state change timestamp
+     */
+    public long face(double x, double y) {
+        return t.face(x, y);
+    }
+
+    /**
+     * Gets direction towards (x,y)
+     *
+     * @param x x coordinate of target location
+     * @param y y coordinate of target location
+     * @return angle in degrees where 0<=angle<360
+     */
+    public double towards(double x, double y) {
+        return t.towards(x, y);
+    }
+
+    /**
+     * Gets the distance to another position.
+     *
+     * @param x x coordinate of target location
+     * @param y y coordinate of target location
+     * @return distance between turtle's current location and another position
+     */
+    public double distance(double x, double y) {
+        return t.distance(x, y);
+    }
+
+    /**
+     * Gets the x coordinate of the turtle.
+     *
+     * @return x coordinate
+     */
+    public double getX() {
+        return t.getX();
+    }
+
+    /**
+     * Gets the y coordinate of the turtle.
+     *
+     * @return y coordinate
+     */
+    public double getY() {
+        return t.getY();
+    }
+
+     /**
+     * Sets the position and direction of a turtle.
+     *
+     * @param x         x coordinate
+     * @param y         y coordinate
+     * @param direction angle counter-clockwise from east in degrees
+     * @return state change timestamp
+     */
+    public long setPosition(double x, double y, double direction) {
+        return t.setPosition(x, y, direction);
+    }
+
+     /**
+     * Sets the position and direction of a turtle.
+     *
+     * @param x x coordinate
+     * @param y y coordinate
+     * @return state change timestamp
+     */
+    public long setPosition(double x, double y) {
+        return t.setPosition(x, y);
+    }
+
+     /**
+     * Sets the width of the turtles path
+     *
+     * @param penWidth width of the turtles path
+     * @return state change timestamp
+     */
+    public long width(double penWidth) {
+        return t.width(penWidth);
+    }
+
+     /**
+     * Picks the turtle's tail up so it won't draw on the screen as it moves.
+     *
+     * @return state change timestamp
+     */
+    public long up() {
+        return t.up();
+    }
+
+     /**
+     * Puts the turtle's tail down so it will draw on the screen as it moves.
+     *
+     * @return state change timestamp
+     */
+    public long down() {
+        return t.down();
+    }
+
+    /**
+     * Sets the turtle's path color.
+     *
+     * @param penColor Color of the turtle's path.
+     * @return state change timestamp
+     */
+    public long penColor(String penColor) {
+        return t.penColor(penColor);
+    }
+
+     /**
+     * Put a copy of the current turtle shape on the canvas.
+     *
+     * @return state change timestamp
+     */
+    public long stamp() {
+        return t.stamp();
+    }
+
+    /**
+     * Put a dot 3 times the size of the penWidth on the canvas.
+     *
+     * @return state change timestamp
+     */
+    public long dot() {
+        return t.dot();
+    }
+
+    /**
+     * Put a dot 3 times the size of the penWidth on the canvas.
+     *
+     * @param color color of dot
+     * @return state change timestamp
+     */
+    public long dot(String color) {
+        return t.dot(color);
+    }
+
+    /**
+     * Clears all the drawing that a turtle has done but all the turtle
+     * settings remain the same. (color, location, direction, shape)
+     */
+    public void clear() {
+        t.clear();
+    }
+
+
+}
