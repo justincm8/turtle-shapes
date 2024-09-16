18e3dfb93ff339e914a1d452f4031fa7f41f5453
justincm8 <80138218+justincm8@users.noreply.github.com>
2024-09-16 16:18:55 +0000

    Mon, Sep 16, 2024, 4:18 PM +00:00

--- TurtleShapes.java
+++ TurtleShapes.java
@@ -1,16 +1,19 @@
 import static twrapper.TurtleWrapper.*;
 
 void main() {
-    System.out.println("Hello");
+    hello();
     penColor("red");
     speed(150);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+}
 
-    forward(100);
-    right(90);
-    forward(100);
-    right(90);
-    forward(100);
-    right(90);
-    forward(100);
-    right(90);
+void hello() {
+    System.out.println("Hi, hello");
 }
